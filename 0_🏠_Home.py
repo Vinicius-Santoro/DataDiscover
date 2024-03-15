d3ca0a400c0b5761ab05e708a9445db6b58e8639
Vinicius-Santoro <viniciussantoro@outlook.com>
2024-03-14 23:26:49 -0300

    feat: :rocket: adiciona texto de boas vindas

--- 0_🏠_Home.py
+++ 0_🏠_Home.py
@@ -1,8 +1,10 @@
 import streamlit as st
+from PIL import Image
 
 st.set_page_config(
     page_title="DataDiscover",
     page_icon="🏠",
+    initial_sidebar_state="expanded",
 )
 
-st.title("DataDiscover")
+st.write('# Bem vindo ao <span style="color: #ff6200">DataDiscover</span>', unsafe_allow_html=True)
