e8bdee6649d063a7a6994acae1e23d8bdf8c6ce6
Vinicius-Santoro <viniciussantoro@outlook.com>
2024-03-14 23:27:56 -0300

    feat: :rocket: adiciona descrição do que é nosso proejto

--- 0_🏠_Home.py
+++ 0_🏠_Home.py
@@ -10,3 +10,10 @@ st.set_page_config(
 st.write('# Bem vindo ao <span style="color: #ff6200">DataDiscover</span>', unsafe_allow_html=True)
 
 st.write('#### O que é nosso projeto?', unsafe_allow_html=True)
+
+# Não utilizar markdown com cores preto ou branco por conta do dark/light mode
+st.markdown("<p style='text-align: justify;'>\
+            O DataDiscover é um site que centraliza três ferramentas projetadas especificamente \
+            para estudantes e entusiastas de ciência de dados. Nosso objetivo é simplificar e aprimorar \
+            a experiência de aprendizado e prática nessa área. \
+</p>", unsafe_allow_html=True)
