07d4de1f8b868b77ac7bf799fd6d9aa799d16764
Vinicius-Santoro <viniciussantoro@outlook.com>
2024-04-23 23:49:04 -0300

    warning: :adiciona kaggle api

--- pages/1_🔎_Motor de Busca.py
+++ pages/1_🔎_Motor de Busca.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import pandas as pd
+# from streamlit.server.server import Server
 from kaggle.api.kaggle_api_extended import KaggleApi
 
 # Configurar a API do Kaggle
@@ -12,30 +12,26 @@ def search_datasets(query):
     return datasets
 
 # Layout do aplicativo
-st.title('Motor de Busca de Dados do Kaggle')
-
-# Inicializar a variável datasets como uma lista vazia
-datasets = []
+st.title('Baixar Conjunto de Dados do Kaggle')
 
 # Entrada de texto para o usuário inserir o termo de busca
-search_term = st.text_input('Digite o termo de busca:')
+search_term = st.text_input('Digite o termo de busca por conjunto de dados no Kaggle:')
 
-# Botão de busca
-if st.button('Buscar'):
+if search_term == "":
+    st.info('Esperando um arquivo excel ou csv ser inserido.')
+    st.button('Buscar')
+else:
+    # Botão de busca
     st.write(f'Buscando por "{search_term}"...')
     datasets = search_datasets(search_term)
-    st.write(f'Encontrados {len(datasets)} conjuntos de dados:')
-    for dataset in datasets:
-        st.write(dataset.ref)
-
-# # Mostrar informações sobre um conjunto de dados selecionado
-dataset_selected = st.selectbox('Selecione um conjunto de dados:', [dataset.ref for dataset in datasets])
-# if dataset_selected:
-#     st.write(f'Informações sobre o conjunto de dados selecionado ({dataset_selected}):')
-#     dataset_info = api.dataset_list_files(dataset_selected)
-#     for file_info in dataset_info.files:  # Acessando a propriedade 'files' do objeto 'ListFilesResult'
-#         st.write(f'Nome do arquivo: {file_info.name}, Tamanho: {file_info.size}')
-#     # Se o usuário quiser baixar o conjunto de dados
-#     if st.button('Baixar conjunto de dados'):
-#         api.dataset_download_files(dataset_selected, unzip=True)
-#         st.write('Conjunto de dados baixado com sucesso!')
+    dataset_names = [dataset.ref for dataset in datasets]
+    selected_dataset = st.selectbox('Selecione um conjunto de dados:', dataset_names) if datasets else None
+    if selected_dataset:
+        st.write(f'Você selecionou o conjunto de dados: {selected_dataset}')
+        dataset_info = api.dataset_list_files(selected_dataset)
+        for file_info in dataset_info.files:  # Acessando a propriedade 'files' do objeto 'ListFilesResult'
+            # st.write(f'Nome do arquivo: {file_info.name}, Tamanho: {file_info.size}')
+        if st.button('Baixar conjunto de dados'):
+            api.dataset_download_files(selected_dataset, unzip=True)
+            st.write('Conjunto de dados baixado com sucesso!')
+            # Server.get_current()._reloader.reload()
