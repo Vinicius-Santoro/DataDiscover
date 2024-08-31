ab768213d89d832917e006b6cef4061fb2dd0a58
Vinicius-Santoro <viniciussantoro@outlook.com>
2024-08-31 16:49:15 -0300

    feat: :rocket: homepage concluída!

--- 0_🏠_Home.py
+++ 0_🏠_Home.py
@@ -10,52 +10,75 @@ st.set_page_config(
 )
 
 # Texto de boas vindas.
-st.write('# Bem vindo ao <span style="color: #ff6200">DataDiscover</span>', unsafe_allow_html=True)
+st.write('# Bem vindo ao <span style="color: #ff6200">DataDiscover</span>',
+         unsafe_allow_html=True)
 
 # Texto de descrição do projeto.
 st.markdown("<p style='text-align: justify;'>\
-            O DataDiscover é um site que centraliza quatro ferramentas projetadas especificamente \
-            para estudantes e entusiastas de ciência de dados. Nosso objetivo é simplificar e aprimorar \
-            a experiência de aprendizado e prática nessa área. \
+            O DataDiscover é um site que centraliza cinco ferramentas\
+            projetadas especificamente para estudantes e entusiastas de\
+            ciência de dados. Nosso objetivo é simplificar e aprimorar a\
+            experiência de aprendizado e prática nessa área.\
 </p>", unsafe_allow_html=True)
 
 # Texto de recursos principais.
 st.write('#### Recursos principais', unsafe_allow_html=True)
 st.markdown("<p style='text-align: justify;'>\
-        Clique em cada uma das ferramentas para saber mais. \
+        Clique em cada uma das ferramentas para saber mais.\
 </p>", unsafe_allow_html=True)
 
 # Texto de explicação do motor de busca, com expander.
 with st.expander("🔎 Motor de Busca"):
     st.markdown("<p style='text-align: justify;'>\
-        O motor de busca permite acessar os principais artigos publicados no Google Scholar.  \
-        Além disso, são apresentados dois indicadores que oferecem uma visão analítica das distribuições.\
+        O motor de busca permite que você pesquise artigos acadêmicos\
+        no Google Scholar, oferecendo também uma visão analítica dos\
+        principais autores e sites relevantes. É ideal para quem precisa\
+        explorar rapidamente as principais publicações em um campo específico\
     </p>", unsafe_allow_html=True)
     
 # Texto de explicação de extração de dados, com expander.
 with st.expander("🎲 Extração de Dados"):
     st.markdown("<p style='text-align: justify;'>\
-            A extração de dados permite obter bases de dados diretamente do Kaggle, \
-            proporcionando dataframes prontos para testes com as demais ferramentas do DataDiscover.\
+        Nesta tela, você pode pesquisar e baixar conjuntos de dados\
+        diretamente do Kaggle em formatos CSV e XLSX. A interface facilita\
+        o acesso rápido a uma variedade de dados para análise ou treinamento\
+        de modelos.\
     </p>", unsafe_allow_html=True)
 
 # Texto de explicação da análise exploratória de dados, com expander.
 with st.expander("📊 Análise Exploratória de Dados"):
     st.markdown("<p style='text-align: justify;'>\
-        A ferramenta de análise exploratória de dados é interessante para analistas e cientistas de dados porque\
-        é possível importar arquivos Excel ou CSV contendo dados brutos e obter uma análise detalhada do dataframe.\
-        A ferramenta revela padrões, tendências e insights ocultos, permitindo uma compreensão mais profunda da estrutura dos dados.\
-        Com base nessa análise, você pode tomar decisões informadas sobre limpeza, transformação e visualização dos dados.\
+        Esta tela oferece uma análise geral e estatística\
+        detalhada dos dados em um conjunto selecionado, permitindo ao usuário\
+        entender melhor as características e tendências antes de qualquer\
+        modelagem.\
     </p>", unsafe_allow_html=True)
 
 # Texto de explicação do relatório de performance do modelo, com expander.
 with st.expander("📃 Relatório de Performance de Modelo"):
     st.markdown("<p style='text-align: justify;'>\
-            Imagine que você está construindo modelos de regressão para resolver um problema específico. \
-         O relatório de performance de modelo te auxilia nesse momento. \
-         Basta inserir um arquivo xlsx ou CSV e você descobrirá como seu dataframe se comportaria em \
-         mais de 20 diferentes modelos de machine learning. Essa análise inclui as métricas de avaliação, \
-         <b>R-Quadradro Ajustado</b>, <b>R-Quadradro</b> e <b>RMSE</b>, permitindo que você escolha o modelo mais adequado para o seu cenário. \
+        Aqui, você pode comparar diferentes modelos de\
+        aprendizado de máquina para determinar qual terá o\
+        melhor desempenho com o seu conjunto de dados,\
+        facilitando a escolha do modelo mais eficaz.\
+    </p>", unsafe_allow_html=True)
+
+# Texto de explicação do motor de busca, com expander.
+with st.expander("😄 Análise de Sentimento"):
+    st.markdown("<p style='text-align: justify;'>\
+        Nesta tela, você pode carregar uma base\
+        de dados e selecionar uma coluna para realizar\
+        a classificação de sentimentos (positivo, negativo ou neutro),\
+        obtendo insights sobre a opinião expressa nos textos.\
+    </p>", unsafe_allow_html=True)
+
+# Texto de explicação do motor de busca, com expander.
+with st.expander("📦 Gerencimento do Banco de Dados"):
+    st.markdown("<p style='text-align: justify;'>\
+        Esta tela permite visualizar e gerencie todas\
+        as bases de dados armazenadas em um banco de dados não\
+        relacional, incluindo a opção de excluir conjuntos de\
+        dados desnecessários.\
     </p>", unsafe_allow_html=True)
 
 st.write('#### Possíveis perguntas', unsafe_allow_html=True)
@@ -65,10 +88,26 @@ message.write("Por que esse projeto foi desenvolvido?")
 
 message = st.chat_message("assistant")
 message.write("\
-        Esse projeto foi desenvolvido como requisito para obtenção de nota para a disciplina \
-        Projeto Integrador III, da Faculdade de Tecnologia de São Paulo (Fatec),\
-        do curso de ciência de dados, quarto semestre, período noturno.\
-              ")
+    Esse projeto criado para centralizar e facilitar o acesso\
+    a ferramentas essenciais para estudantes e entusiastas de\
+    ciência de dados, oferecendo uma plataforma única onde eles\
+    podem aprender, praticar e aprimorar suas habilidades de\
+    forma simplificada e eficiente.\
+")
+
+message = st.chat_message("user")
+message.write("Como esse projeto pode ajudar um estudante de dados? ")
+
+message = st.chat_message("assistant")
+message.write("\
+    O DataDiscover ajuda estudantes de dados ao fornecer\
+    ferramentas integradas que cobrem desde a busca e\
+    análise de artigos acadêmicos até a extração de dados, \
+    nálise exploratória, avaliação de modelos de aprendizado\
+    de máquina, e análise de sentimentos. Isso permite que\
+    os estudantes ganhem experiência prática e consolidem\
+    seus conhecimentos em um ambiente centralizado.\
+")
 
 message = st.chat_message("user")
 message.write("Como o projeto foi desenvolvido?")
@@ -81,7 +120,7 @@ message.write("\
         **• Streamlit:** biblioteca utilizada para desenvolver a interface.\n\n  \
         **• Git:** versionar o código fonte.\n\n\
         **• Github:** hospedar o código fonte.\n\n\
-              ")
+")
 
 st.write('#### Quer saber mais? Baixe nossa documentação completa do projeto 🚀', unsafe_allow_html=True)
 
